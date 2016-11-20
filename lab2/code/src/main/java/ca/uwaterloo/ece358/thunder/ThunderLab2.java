b3299ac5291c3ee0fa2bcf0b0513ca7306b3718a
Ramie Raufdeen <me@ramie.ca>
2016-11-20 18:56:39 -0500

    added simulator class and first run of simulations

--- lab2/code/src/main/java/ca/uwaterloo/ece358/thunder/ThunderLab2.java
+++ lab2/code/src/main/java/ca/uwaterloo/ece358/thunder/ThunderLab2.java
@@ -1,5 +1,33 @@
 package ca.uwaterloo.ece358.thunder;
 
-public class ThunderLab2 {
+import ca.uwaterloo.ece358.thunder.data.Simulator;
 
+public class ThunderLab2 {
+    private static final int NUM_TICKS = 100000000;
+    private static final int PACKET_LENGTH = 1500;
+
+    public static void main(String[] args) {
+        int[] n = {20, 40, 60, 80, 100};
+        int[] lambdas = {6, 20};
+
+        for (int i = 0; i < lambdas.length; i++) {
+            for (int j = 0; j < n.length; j++) {
+                Simulator s = new Simulator(NUM_TICKS, n[j], lambdas[i], PACKET_LENGTH, 0);
+                s.run();
+                s.printMetrics();
+            }
+        }
+
+        System.out.println("=============================================");
+
+        double[] p = {0.01, 0.1, 1};
+        int[] lambdas2 = {1, 3, 5, 7, 10};
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < lambdas2.length; j++) {
+                Simulator s = new Simulator(NUM_TICKS, 30, lambdas2[j], PACKET_LENGTH, p[i]);
+                s.run();
+                s.printMetrics();
+            }
+        }
+    }
 }
--- lab2/code/src/main/java/ca/uwaterloo/ece358/thunder/data/Simulator.java
+++ lab2/code/src/main/java/ca/uwaterloo/ece358/thunder/data/Simulator.java
@@ -1,32 +1,87 @@
 package ca.uwaterloo.ece358.thunder.data;
 
 import ca.uwaterloo.ece358.thunder.data.node.NetworkNode;
-import com.sun.corba.se.impl.orbutil.graph.Graph;
+import ca.uwaterloo.ece358.thunder.data.node.PPersistentNetworkNode;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Simulator <T extends NetworkNode> {
-    private List<T> nodes;
-    private Graph network;
+public class Simulator {
+    private List<NetworkNode> nodes;
+    private Matrix network;
     private int packetLength;
     private long ticks;
     private double p;
     private int N;
     private long lambda;
-    private long networkSpeed;
 
-    public Simulator(long ticks, int N, long lambda, long networkSpeed, int packetLength, double p){
-        this.nodes = new ArrayList<T>();
-        this.N =N;
+    public Simulator(long ticks, int N, long lambda, int packetLength, double p) {
+        this.nodes = new ArrayList<NetworkNode>();
+        this.N = N;
         this.lambda = lambda;
-        this.networkSpeed = networkSpeed;
         this.packetLength = packetLength * 8;
         this.ticks = ticks;
         this.p = p;
+        this.network = new Matrix();
 
-        for(int i =0; i<N; i++){
-            this.nodes.add();
+
+    }
+
+    public void run() {
+        this.nodes.clear();
+
+        for (int i = 0; i < N; i++) {
+            this.nodes.add(generateNode(i, lambda, p));
         }
+
+        for (int i = 0; i < this.ticks; i++) {
+            for (NetworkNode n : this.nodes) {
+                n.process();
+            }
+            this.network.reset();
+        }
+    }
+
+    public void printMetrics(){
+        int totalSent = 0;
+        long delayTime = 0;
+        for (NetworkNode n : nodes) {
+            totalSent += n.getRequestsCompleted();
+            delayTime += n.getDelayTime();
+        }
+
+        double throughput = (double)totalSent / (double)ticks;
+        double averageDelay = (double)delayTime / (double)totalSent;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.N);
+        sb.append(",");
+        sb.append(this.lambda);
+        sb.append(",");
+        sb.append(this.p);
+        sb.append(",");
+        sb.append(totalSent);
+        sb.append(",");
+        sb.append(ticks);
+        sb.append(",");
+        sb.append(throughput);
+        sb.append(",");
+        sb.append(averageDelay);
+
+        System.out.println(sb.toString());
+    }
+
+    private NetworkNode generateNode(int i, long lambda, double p) {
+        if (p == 0) {
+            return new NetworkNode(this.network, computePropogation(i), lambda, this.packetLength, p);
+        } else {
+            return new PPersistentNetworkNode(this.network, computePropogation(i), lambda, this.packetLength, p);
+        }
+    }
+
+    private static long computePropogation(int index) {
+        double distance = (double) index * 10.0;
+        double delaySeconds = distance / 2E8;
+        return (long) (delaySeconds * 1E6);
     }
 }
